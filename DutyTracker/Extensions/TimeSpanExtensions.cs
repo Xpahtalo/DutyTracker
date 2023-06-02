b44a6b533ece737fb393086aa78d2d3345cf9499
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-06-01 18:28:02 -0700

    Fix minor display bug

--- DutyTracker/Extensions/TimeSpanExtensions.cs
+++ DutyTracker/Extensions/TimeSpanExtensions.cs
@@ -8,7 +8,7 @@ internal static class TimeSpanExtensions
     public static string MinutesAndSeconds(this TimeSpan timeSpan)
     {
         var stringBuilder = new StringBuilder();
-        stringBuilder.Append(timeSpan.Minutes);
+        stringBuilder.Append(timeSpan.TotalMinutes);
         stringBuilder.Append(':');
         if (timeSpan.Seconds < 10)
             stringBuilder.Append('0');
@@ -22,7 +22,7 @@ internal static class TimeSpanExtensions
         var stringBuilder = new StringBuilder();
         if (timeSpan.TotalHours > 1)
         {
-            stringBuilder.Append(timeSpan.Hours);
+            stringBuilder.Append(timeSpan.TotalHours);
             stringBuilder.Append(':');
         }
 
